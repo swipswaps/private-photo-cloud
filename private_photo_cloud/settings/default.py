0ad615a2fb6950541c246dd9270177f29040ab5a
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-10-30 21:59:19 +0100

    Renamed settings file

--- private_photo_cloud/settings/default.py
+++ private_photo_cloud/settings/default.py
@@ -1,5 +1,9 @@
 import dj_database_url
 
-from .defaults import *
+from .factory_defaults import *
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+INSTALLED_APPS += [
+    'photo',
+]
