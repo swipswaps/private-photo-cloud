62bed4f956fd6a92d8d39ab5ee419d4822055ccb
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-10-31 23:22:24 +0100

    Started toying with React JS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        var generatePhoto = function(n) {
+          return {
+            name: String.fromCharCode(n + (n < 26 ? 97 : (65-26))),
+            id: n
+          };
+        };
+
+        this.state = {
+            photos: Array.apply(null, Array(52)).map((_, n) => generatePhoto(n))
+        };
+
+        this.render = this.render.bind(this);
+        this.renderPhotos = this.renderPhotos.bind(this);
+        this.renderPhoto = this.renderPhoto.bind(this);
+        this.onClick = this.onClick.bind(this);
+    }
+
+  onClick() {
+    alert('clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo10" />
-          <h2>Say hi to React JS!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderPhotos()}
       </div>
+    );
+  }
+
+  renderPhotos() {
+    return (
+      <div className="photosList">
+        {this.state.photos.map(photo => this.renderPhoto(photo))}
+      </div>
+      );
+  }
+
+  renderPhoto(photo) {
+    return (
+      <div className="photo" onClick={this.onClick} key={photo.id}>{photo.name}</div>
     );
   }
 }
