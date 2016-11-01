a5abfde4012b7786254baeaec8f68b10ae471c99
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-11-01 14:50:43 +0100

    Moved helper methods to helpers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Helper from './helpers.js';
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -23,33 +25,9 @@ class App extends Component {
         this.onClick = this.onClick.bind(this);
     }
 
-  hex(buffer) {
-    var hexCodes = [];
-    var view = new DataView(buffer);
-    var padding = '00000000';
-    for (var i = 0; i < view.byteLength; i += 4) {
-      // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-      // toString(16) will give the hex representation of the number without padding
-      var value = view.getUint32(i).toString(16);
-      // We use concatenation and slice for padding
-      hexCodes.push((padding + value).slice(-padding.length));
-    }
-
-    // Join all the hex strings into one
-    return hexCodes.join("");
-  }
-
-  sha1(text) {
-    var that = this;
-    return crypto.subtle.digest("SHA-1", new TextEncoder("utf-8").encode(text)
-      ).then(function(buffer) {
-        return that.hex(buffer);
-      });
-  }
-
   onClick(e) {
     var text = e.target.innerText;
-    this.sha1(text).then(function(hex){
+    Helper.sha1(text).then(function(hex){
       alert(`clicked: ${text}: ${hex}`);
     })
   }
