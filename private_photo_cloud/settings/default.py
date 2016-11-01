7c978a5103f49996c6827b1500847ae65b2e56c8
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-11-01 15:57:51 +0100

    Defined used context processors

--- private_photo_cloud/settings/default.py
+++ private_photo_cloud/settings/default.py
@@ -16,3 +16,15 @@ LOGIN_URL = 'login'
 TEMPLATES[0]['DIRS'] += [
     os.path.join(BASE_DIR, 'templates'),
 ]
+
+TEMPLATES[0]['OPTIONS']['context_processors'] = [
+    'django.template.context_processors.debug',
+    'django.template.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.template.context_processors.i18n',
+    'django.template.context_processors.media',
+    'django.template.context_processors.static',
+    'django.template.context_processors.csrf',
+    'django.template.context_processors.tz',
+]
