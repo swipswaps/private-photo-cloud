a9499dd6e87cc9424c86cea9454f557d1016cc49
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-11-26 23:34:16 +0100

    Added a link to upload from catalog page

--- private_photo_cloud/urls.py
+++ private_photo_cloud/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^login/', login, name='login'),
     url(r'^logout/', logout, name='logout'),
     url(r'^admin/', admin.site.urls),
-    url(r'^upload/', include('upload.urls')),
+    url(r'^upload/', include('upload.urls', namespace='upload')),
 
     url(r'^', include('catalog.urls')),
 
--- upload/urls.py
+++ upload/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.upload),
+    url(r'^$', views.upload, name='upload'),
     url(r'^file/$', views.upload_file),
     url(r'^media/(?P<alg>sha1)_(?P<digest>[0-9a-f]{40})_(?P<size>\d+)/$', views.check_present),
 ]
