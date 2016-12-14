e7e9329ec1d99d7d17e0bee856c7a78a12887e4c
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-12-14 23:15:15 +0100

    Replaced deprecated functional login and logout methods

--- private_photo_cloud/urls.py
+++ private_photo_cloud/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^login/', login, name='login'),
-    url(r'^logout/', logout, name='logout'),
+    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^upload/', include('upload.urls', namespace='upload')),
 
