c32ceaa08ba1713376f7527f948de3d9619b7a5c
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-12-18 22:23:22 +0100

    Added debug info for consumers

--- upload/consumers.py
+++ upload/consumers.py
@@ -6,12 +6,14 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 @channel_session_user_from_http
 def ws_add(message):
     # Add them to the right group
+    # print(f'added WS#{message.reply_channel} to upload-{message.user.id}')
     Group(f'upload-{message.user.id}').add(message.reply_channel)
 
 
 # Connected to websocket.receive
 @channel_session_user
 def ws_message(message):
+    # print(f'got message to upload-{message.user.id}')
     Group(f'upload-{message.user.id}').send({
         "text": f"{message['text']}@{message.user.id}",
     })
@@ -20,4 +22,5 @@ def ws_message(message):
 # Connected to websocket.disconnect
 @channel_session_user
 def ws_disconnect(message):
+    # print(f'removed WS#{message.reply_channel} from upload-{message.user.id}')
     Group(f'upload-{message.user.id}').discard(message.reply_channel)
