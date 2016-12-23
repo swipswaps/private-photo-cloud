2ec0f6b811819fafb736ba2f6797f6e33551e585
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2016-12-23 22:27:42 +0100

    Pass extra parameters to messages scripts

--- compilemessages.sh
+++ compilemessages.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 
 for dir in $(find "$(pwd)" -name locale -type d); do
     cd "${dir}/../"
-    PYTHONWARNINGS=ignore ../manage.py compilemessages
+    PYTHONWARNINGS=ignore ../manage.py compilemessages "$@"
 done
--- makemessages.sh
+++ makemessages.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 
 for dir in $(find "$(pwd)" -name locale -type d); do
     cd "${dir}/../"
-    PYTHONWARNINGS=ignore ../manage.py makemessages --all --no-wrap
+    PYTHONWARNINGS=ignore ../manage.py makemessages --all --no-wrap "$@"
 done
