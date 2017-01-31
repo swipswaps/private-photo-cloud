c1b345b0c675e431dbec50aa309c5258541da759
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2017-02-01 00:13:34 +0100

    Started migrate to React for catalog

--- frontend/js/catalog.js
+++ frontend/js/catalog.js
@@ -11,19 +11,61 @@ import ReactDOM from 'react-dom';
 
 import {onDomLoaded} from './helpers';
 
-class CatalogApp extends Component {
+function CatalogApp() {
+    return (
+        <div className="App">
+            Months:
+            <AllYearsCatalog />
+        </div>
+    );
+}
+
+class AllYearsCatalog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            months: []
+        };
+    }
     componentDidMount() {
-        // it is the right place to load data (with low overhead)
+        fetch('/images/months.json', {credentials: 'same-origin'}
+        ).then(response => response.json()
+        ).then(data => {
+            console.log(data);
+            this.setState({
+                months: data.months.map(m => ({month: m, expanded: false}))
+            });
+        });
     }
 
     render() {
         return (
-            <div className="App">
-                Catalog
-            </div>
+            <ul>
+                {this.state.months.map(month => (
+                    <li key={month.month} onClick={this.toggleMonth.bind(this, month.month)}>
+                        {month.month}
+                        {month.expanded ? (<MonthCatalog key={month.month} month={month.month} />) : null}
+                    </li>
+                ))}
+            </ul>
         );
     }
+
+    toggleMonth(month) {
+        this.setState({
+            months: this.state.months.map(m => m.month === month ? {...m, expanded: !m.expanded} : m)
+        });
+    }
 }
+
+function MonthCatalog(props) {
+    return (
+        <ul>
+            <li>{props.month}</li>
+        </ul>
+    );
+}
+
 
 onDomLoaded().then(() => {
     ReactDOM.render(<CatalogApp />, document.getElementById('root'));
