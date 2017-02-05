c057e95031c272b108c531d3cc1bbba747a72678
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2017-02-05 23:58:26 +0100

    Show media in month component

--- frontend/js/catalog.js
+++ frontend/js/catalog.js
@@ -11,6 +11,12 @@ import ReactDOM from 'react-dom';
 
 import {onDomLoaded} from './helpers';
 
+
+const HIDPI_SCALE = 2;
+const MEDIA_IMAGE = 1;
+const MEDIA_VIDEO = 2;
+
+
 function CatalogApp() {
     return (
         <div className="App">
@@ -31,9 +37,8 @@ class AllYearsCatalog extends Component {
         fetch('/images/months.json', {credentials: 'same-origin'}
         ).then(response => response.json()
         ).then(data => {
-            console.log(data);
             this.setState({
-                months: data.months.map(m => ({month: m, expanded: false}))
+                months: data.months.map(m => ({...m, expanded: false, media: []}))
             });
         });
     }
@@ -41,19 +46,34 @@ class AllYearsCatalog extends Component {
     render() {
         return (
             <ul>
-                {this.state.months.map(month => (
-                    <li key={month.month} onClick={this.toggleMonth.bind(this, month.month)}>
-                        {month.month}
-                        {month.expanded ? (<MonthCatalog key={month.month} month={month.month} />) : null}
+                {this.state.months.map(m => (
+                    <li key={m.month} onClick={this.toggleMonth.bind(this, m.month, !m.expanded)}>
+                        {m.month} ({m.num})
+                        {m.expanded ? (<MonthCatalog key={m.month} month={m.month} media={m.media} />) : null}
                     </li>
                 ))}
             </ul>
         );
     }
 
-    toggleMonth(month) {
+    toggleMonth(month, expand) {
+        if(expand) {
+            this.loadMonth(month);
+        }
         this.setState({
-            months: this.state.months.map(m => m.month === month ? {...m, expanded: !m.expanded} : m)
+            months: this.state.months.map(m => m.month === month ? {...m, expanded: expand, media: []} : m)
+        });
+    }
+
+    loadMonth(month) {
+        fetch(`/images/${month}.json`, {
+            credentials: 'same-origin',
+        }).then(function(response){
+            return response.json();
+        }).then(data => {
+            this.setState({
+                months: this.state.months.map(m => m.month === month ? {...m, media: data.media} : m)
+            });
         });
     }
 }
@@ -61,8 +81,36 @@ class AllYearsCatalog extends Component {
 function MonthCatalog(props) {
     return (
         <ul>
-            <li>{props.month}</li>
+            {props.media.map(media => (<Media media={media} key={media.id} />))}
         </ul>
+    );
+}
+
+function Media(props) {
+    let media = props.media;
+    let title = [];
+    for(let k of Object.keys(media)) {
+        title.push(`${k}: ${media[k]}`);
+    }
+
+    return (
+        <li>
+            <div id={`media-${media.id}`}
+                 className={`media ${MEDIA_TYPES[media.media_type]}`}
+                 style={{
+                     backgroundImage: `url(${MEDIA_URL + media.thumbnail})`,
+                     width: `${media.thumbnail_width / HIDPI_SCALE}px`,
+                     height: `${media.thumbnail_height / HIDPI_SCALE}px`
+                 }}
+                 title={title.join("\n")}
+                 data-id={media.id}
+                 data-content={media.content}
+                 data-media_type={media.media_type}
+                 data-screenshot={media.screenshot || null}
+            >
+                {media.media_type === MEDIA_VIDEO ? (<img src={`${STATIC_URL}play.svg`} className="play-video" />) : null}
+            </div>
+        </li>
     );
 }
 
