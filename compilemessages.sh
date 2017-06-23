97a7b2b2c1a2ce43a7ff37a752eaacc11a5fe181
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2017-06-24 00:07:58 +0200

    Added TODO note

--- compilemessages.sh
+++ compilemessages.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname "$0")"
 
+# TODO: Make a single translation file
+
 for dir in $(find "$(pwd)" -name locale -type d); do
     cd "${dir}/../"
     PYTHONWARNINGS=ignore ../manage.py compilemessages "$@"
